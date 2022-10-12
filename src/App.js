9eaed430ee03c1ac17aca815de23e92f160492d0
stefanappdev <stevenbrown2dn@gmail.com>
2022-10-12 08:08:04 -0500

    First commit 12/09/2022 08:07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import{ BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+
+import Favs from "./components/favs.js"
+import Home from "./components/home.js"
+import Places from "./components/places.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="places">Places</Link>
+          <Link to="Favs">Favs</Link>
+
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/places" element={<Places />}/>
+            <Route path="/favs" element={<Favs />} />
+        </Routes>
+     </Router>
     </div>
-  );
+
+    );
 }
 
 export default App;
---
+++ src/components/places.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+function Places(){
+
+	return(
+
+
+		<h1>this is the Places page</h1>
+
+		)
+
+}
+
+
+export default Places;
